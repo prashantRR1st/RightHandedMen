8c2d4a511c53154210f0681d5a6847d486ca848d
Zaeem Mohamed <zaeem@facedrivehealth.com>
2022-01-30 22:54:59 -0500

    duty cycle pwm with safety

--- RHMega/src/main.cpp
+++ RHMega/src/main.cpp
@@ -1,14 +1,58 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(13, OUTPUT);
+// driver pins
+int pulPin = 13; //980 Hz PWM on Mega
+int dirPin = 7;
+int dc = 200;
+int delayTime = 2000;
+
+int encoderOutput;
+unsigned long counter = 0; // counting encoder pulses
+
+bool run = false;
+
+void setup(){
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    Serial1.begin(9600);
+    Serial.println();
+    Serial.println("Starting...");
+
+    //motor driver
+    pinMode(pulPin, OUTPUT);
+    pinMode(dirPin, OUTPUT);
+    digitalWrite(dirPin, HIGH);
+    //analogWrite(pulPin, 127); // 0 - 0% duty cycle, 255 - 100% duty cycle
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(13, LOW); delay(1000);
-  digitalWrite(13, HIGH); delay(1000);
-  Serial.println("ooga booga");
+void loop(){
+    // receive command from serial terminal
+    if (Serial.available() > 0)    {
+        char inByte = Serial.read();
+        if (inByte == 'w')            run = true;
+        else if (inByte == 's')       run = false;
+        else Serial.println(inByte);
+    }
+
+    if (run) {
+        digitalWrite(pulPin, HIGH);
+        delayMicroseconds(dc);
+        digitalWrite(pulPin, LOW);
+        delayMicroseconds(delayTime - dc);
+    }
+    else {
+        digitalWrite(pulPin, LOW);
+    }
+
+    // Read encoders
+    /*Serial.println("Serial 1 avail");
+    Serial.println(Serial1.available());
+    Serial.println("Serial 0 avail");
+    Serial.println(Serial.available());
+    delay(1000);
+    if (Serial1.available() > 0) {
+        encoderOutput = Serial1.read();
+    }
+  
+    Serial.println(encoderOutput);*/
 }
