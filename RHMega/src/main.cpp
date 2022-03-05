6a68a98e8072e6ccc08a4ec8ca03dce315623465
Hsifeulbhsifder <zaeemkmohamed@gmail.com>
2022-03-05 02:26:29 -0500

    motor stuff

--- RHMega/src/main.cpp
+++ RHMega/src/main.cpp
@@ -83,3 +83,32 @@ void loop(){
   
     Serial.println(encoderOutput);*/
 }
+
+
+/**
+ * VEX Robotics Victor 888 Motor Controller The Vex Robotics Victor 884 Motor Controller can also be
+ * used with this class but may need to be calibrated per the Victor 884 user manual.
+ *
+ * <p>Note that the Victor uses the following bounds for PWM values. These values were determined
+ * empirically and optimized for the Victor 888. These values should work reasonably well for Victor
+ * 884 controllers also but if users experience issues such as asymmetric behavior around the
+ * deadband or inability to saturate the controller in either direction, calibration is recommended.
+ * The calibration procedure can be found in the Victor 884 User Manual available from VEX Robotics:
+ * http://content.vexrobotics.com/docs/ifi-v884-users-manual-9-25-06.pdf
+ *
+ * <ul>
+ *   <li>2.027ms = full "forward"
+ *   <li>1.525ms = the "high end" of the deadband range
+ *   <li>1.507ms = center of the deadband range (off)
+ *   <li>1.490ms = the "low end" of the deadband range
+ *   <li>1.026ms = full "reverse"
+ * </ul>
+ */
+
+void configMotor(){
+    
+}
+
+void writeMotor(int speed){
+
+}
