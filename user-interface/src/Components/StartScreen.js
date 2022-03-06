8049fda8dfb01d2fba4b439691de06726dcd662e
Prashant Revaneti <prashxlr8@gmail.com>
2022-03-06 14:09:19 -0500

    populate start screen with options

--- user-interface/src/Components/StartScreen.js
+++ user-interface/src/Components/StartScreen.js
@@ -1,18 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Header from "./Header";
-import Button from '@mui/material/Button';
+import { Button, Grid } from '@mui/material';
+import WorkoutSelectionCard from "./WorkoutSelectionCard";
+import Typography from '@mui/material/Typography';
 
 export default function StartScreen() {
     return(
         <div>
             <Header></Header>
-            <h1>Welcome to Plyo-Metrics</h1>
-            <Link to="/calibrate">
-                <Button variant="contained">
-                    <span>Begin Calibration</span>
-                </Button>
-            </Link>
+            <Typography gutterBottom variant="h4" component="div">
+                Welcome to Plyo-Metrics
+            </Typography>
+            <Typography gutterBottom variant="h5" component="div">
+                Please Select a Workout:
+            </Typography>
+            <Grid 
+                container
+                direction="row"
+                alignItems="center"
+                justifyContent="center"
+                style={{padding: '1%'}}
+            >
+                <Grid item xs={5} style={{padding: '1%'}}>
+                    <WorkoutSelectionCard imgSrc="walPush.JPG" title="Wall Push Up"></WorkoutSelectionCard>
+                </Grid>
+                <Grid item xs={5} style={{padding: '1%'}}>
+                    <WorkoutSelectionCard imgSrc="legPress.JPG" title="Leg Press"></WorkoutSelectionCard>
+                </Grid>
+                <Grid item xs={5} style={{padding: '1%'}}>
+                    <WorkoutSelectionCard imgSrc="pushUp.JPG" title="Standard Push Up"></WorkoutSelectionCard>
+                </Grid>
+                <Grid item xs={5} style={{padding: '1%'}}>
+                    <WorkoutSelectionCard imgSrc="calfPlyo.JPG" title="Calf Raises"></WorkoutSelectionCard>
+                </Grid>
+            </Grid>
         </div>
     );
 };
