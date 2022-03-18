564dfbd76f60f6584be8e61bc2c93b2edcd80a28
Prashant Revaneti <prashxlr8@gmail.com>
2022-03-18 10:24:15 -0400

    livestream control output

--- user-interface/src/Components/WorkoutChart.js
+++ user-interface/src/Components/WorkoutChart.js
@@ -16,38 +16,50 @@ export default function WorkoutChart() {
 
     useInterval(() => {
         const fetchData = async () => {
-            const res = await fetch('http://127.0.0.1:8887/tracking_output.json', {mode:'cors'});
-            const json = await res.json();
-            let displayData = [];
-
-            json.forEach((element, index) => {
+            try {
+                const res = await fetch('http://127.0.0.1:8887/tracking_output.json', {mode:'cors'});
+                const json = await res.json();
+                let dataLen = json.length; 
+                
                 let results_len = results['buffer'].length;
-                let profile_index = results_len > index? index : index%results_len;
-                displayData.push(
-                    {
-                        time: index,
-                        live_force: element,
-                        profile: results['buffer'][profile_index]
-                    }
-                );
-            })
-            setData(displayData);
+                let xScale = 5;
+                let startIndex = dataLen>(results_len*xScale)? dataLen-(results_len*xScale) : 0;
+                let truncatedData = json.slice(startIndex, dataLen);
+                let displayData = [];
+
+                truncatedData.forEach((element, index) => {
+                    
+                    let profile_index = results_len > index? index : index%results_len;
+                    displayData.push(
+                        {
+                            time: index,
+                            live_force: element,
+                            profile: results['buffer'][profile_index]
+                        }
+                    );
+                })
+                setData(displayData);
+              }
+              catch(err) {
+                return;
+              }
+            
         };
         fetchData();
       }, 10);
 
 
     return(
-        <div>
-            <ResponsiveContainer width="100%" aspect={3}>
-                <LineChart data={data} margin={{ left: 100, right: 100, top: 30, bottom: 30 }}>
-                    <CartesianGrid />
+        <div style={{backgroundColor: '#121212'}}>
+            <ResponsiveContainer width="99%" aspect={3}>
+                <LineChart data={data} margin={{ left: 80, right: 100, top: 30, bottom: 30 }}>
+                    <CartesianGrid stroke="#404040" />
                         <XAxis dataKey="time (s)" interval={'preserveStartEnd'} />
                     <YAxis></YAxis>
                     <Legend />
                     <Tooltip />
-                    <Line dataKey="live_force" stroke="black" activeDot={{ r: 8 }} />
-                    <Line dataKey="profile" stroke="red" activeDot={{ r: 8 }} />
+                    <Line dataKey="live_force" stroke="#119db0" strokeWidth={2} activeDot={{ r: 8 }} />
+                    <Line dataKey="profile" stroke="#f15b04"  strokeWidth={2} activeDot={{ r: 8 }} />
                 </LineChart>
             </ResponsiveContainer>
             
